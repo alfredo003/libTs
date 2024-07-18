484a98cae584ddf7e9c20d5d851f629aba99dc1f
Alfredo Manuel <59560615+alfredo003@users.noreply.github.com>
2024-07-18 14:34:26 +0000

    updated

--- main.ts
+++
@@ -1,6 +0,0 @@
-import {strrev} from "./lib";
-
-
-let a = strrev("Hello World".split(''));
-
-console.log(a);
---
+++ tests/lib.test.ts
@@ -0,0 +1,6 @@
+/* //test function *strrev*
+
+import {strrev} from "./lib";
+let a = strrev("Hello World".split(''));
+console.log(a);
+*/
