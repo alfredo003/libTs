e8acc0a1c2929bae12a0c876fbb25cfd9ad35ca9
Alfredo Manuel <59560615+alfredo003@users.noreply.github.com>
2024-07-18 15:58:31 +0000

    updated of lib

--- lib.ts
+++ lib.ts
@@ -18,4 +18,51 @@ export function strrev(str:string[]):string
     }
     return (str.join(''));
 }
+export function strchr(s:string[],c:string):number
+{
+    let i:number = 0;
+    while(s[i])
+    {
+        if(s[i] === c) return (i);
+        ++i;
+    }
+    return (-1);
+}
+export function strspn(s:string[],accept:string[]):number
+{
+    let i:number = 0;
 
+    while(s[i])
+    {
+            if(strchr(accept,s[i]) === -1) break;
+         ++i;
+    }
+    return (i);
+}
+export function has_char(s:string[],c:string):number
+{
+    let i:number =0;
+    while(s[i])
+    {
+        if(s[i] === c) return (1);
+        ++i;
+    }
+    return (0);
+}
+export function union(s1:string[],s2:string[]):string
+{
+    let i:number=0;
+    let j:number=0;
+    let seen:string[] = [];
+    
+    while(s1[i])
+    {
+        if(has_char(s2,s1[i]) && !has_char(seen,s1[i]))
+        {
+            seen[j] = s1[i];
+            j++;
+        }
+        i++;
+    }
+    return (seen.join(''));
+}
--- tests/lib.test.ts
+++ tests/lib.test.ts
@@ -1,6 +1,30 @@
 /* //test function *strrev*
 
-import {strrev} from "./lib";
+import {strrev} from "../lib";
 let a = strrev("Hello World".split(''));
 console.log(a);
 */
+
+/*//test function *strchr*
+
+import {strchr} from "../lib";
+let a = strchr("Hello, world!".split(''),"z");
+console.log(a);*/
+
+/*//test function *strchr*
+
+import {strspn} from "../lib";
+let s:string[] = "abcdef".split('');
+let accept:string[] = "abc".split('');
+
+let a = strspn(s,accept);
+console.log(a);*/
+
+//test function *union*
+
+import {union} from "../lib";
+let s:string[] = "padinton".split('');
+let accept:string[] = "paqefwtdjetyiytjneytjoeyjnejeyj".split('');
+
+let a = union(s,accept);
+console.log(a);
